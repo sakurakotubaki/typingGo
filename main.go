d900389e977bf24e51e587fc68f20e19dcb013fd
sakurakotubaki <gitonly543@gmail.com>
2025-11-08 16:18:30 +0900

    hoge
    added: comment
    
    delete: commnet

--- main.go
+++ main.go
@@ -2,45 +2,22 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
-	S string
+type Person struct {
+	Name string
 }
 
-// *がないとスコープの中でした変更は反映されない
-// func changeVertex(v Vertex) {
-// 	v.X = 1000
-// 	fmt.Println("scop in value", v)
-// }
+type Hoge struct {
+	Name string
+}
 
-// *があるとポインターを渡すことができる
-// func changeVertex(v *Vertex) {
-// 	v.X = 1000
-// 	fmt.Println("scop in value", v)
-// }
-
-// (*v).X = 1000 と同じ意味
-func changeVertex(v *Vertex) {
-	(*v).X = 1000
-	fmt.Println("(*v)で自動でポインタの実態を指す", v)
+func (p *Person) Say() {
+	p.Name = "hoge"
+	// %pを使うとポインタのアドレスが表示される
+	fmt.Printf("%p\n", p)
 }
 
 func main() {
-	// v := Vertex{X: 1, Y: 2, S: "hello"}
-	// fmt.Println(v)
-	// v.X = 10
-	// fmt.Println(v.X, v.Y)
-
-	// v6 := new(Vertex)
-	// fmt.Println(v6)サイレントTV
-
-	// newで作成するよりも「&Vertex{}」のようにアドレスをつけた状態で宣言した方がポインタが返ってくることが明示的なためよく使われる。
-	// v7 := &Vertex{}
-	// fmt.Printf("好きだった%s より %d つけvマックス\n", v7.S, v7.X)
-
-	// v := Vertex{X: 1, Y: 2, S: "hello"}
-	v := &Vertex{X: 1, Y: 2, S: "ポインターで値を渡したよ☺️ "}
-	changeVertex(v)
-	fmt.Println(v)
+	p := Person{Name: "fuga"}
+	p.Say()
+	fmt.Println(p.Name)
 }
